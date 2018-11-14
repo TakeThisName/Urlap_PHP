e85c82e63a374c4182e971fa477f06242188d378
avaaris <avaaris@gmail.com>
2018-11-14 19:31:51 +0100

    Urlap_PHP_Belep_R

---
+++ belep.php
@@ -0,0 +1,6 @@
+<?php
+            if (isset($_POST["submit"])){
+                echo $_POST['username']."<br>"; // server oldalo a name értékre van szükség
+                echo $_POST['pwd'];
+            }
+        ?>
--- index.php
+++ index.php
@@ -6,14 +6,11 @@
         <title></title>
     </head>
     <body>
-        <form method="post" action="#"> <!-- "get" <- a címsorban küldi az adatot, a "post" nem; "#" a script helye az adott fájl -->
+        <form method="post" action="belep.php"> <!-- "get" <- a címsorban küldi az adatot, a "post" nem; "#" a script helye az adott fájl -->
             <input type="text" name="username" required placeholder="Felhasználónév">
             <input type="text" name="pwd" required placeholder="Jelszó">
             <input type="submit" value="Belépés" name="submit">
         </form>
-        <?php
-            echo $_POST['username']."<br>"; // server oldalo a name értékre van szükség
-            echo $_POST['pwd'];
-        ?>
+        
     </body>
 </html>
