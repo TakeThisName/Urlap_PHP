889d136abe4f8210deef3f92e151238adcf3cfab
avaaris <avaaris@gmail.com>
2018-11-14 19:52:06 +0100

    Urlap_PHP_Belep_R_Cont

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <form method="post" action="belep.php"> <!-- "get" <- a címsorban küldi az adatot, a "post" nem; "#" a script helye az adott fájl -->
             <input type="text" name="username" required placeholder="Felhasználónév">
             <input type="text" name="pwd" required placeholder="Jelszó">
+            <input type="text" name="pwd2" required placeholder="Jelszó mégegyszer">
             <input type="submit" value="Belépés" name="submit">
         </form>
         
