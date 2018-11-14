bd5bd29d95cce87cc54d96dea4baefe7dbcd90cf
avaaris <avaaris@gmail.com>
2018-11-14 19:49:15 +0100

    Urlap_PHP_Belep_R_Cont

--- belep.php
+++ belep.php
@@ -1,6 +1,17 @@
 <?php
-            if (isset($_POST["submit"])){
-                echo $_POST['username']."<br>"; // server oldalo a name értékre van szükség
-                echo $_POST['pwd'];
+            if (isset($_POST["submit"])){ // server oldalo a name értékre van szükség
+//                echo $_POST['username']."<br>"; 
+//                echo $_POST['pwd'];
+                $un = $_POST["username"];
+                $pwd = $_POST["pwd"];
+                $pwd2 = $_POST["pwd2"];
+                
+                if ($un == "user" && $pwd == "password" && $pwd == $pwd2){
+                    echo "Üdvözöllek ".$un;
+                }
+                 else {
+                        echo "A jelszó helytelen, vagy nem egyezik meg a megerősítéssel!<br>";
+                }
+                echo '<a href="index.php"><br>Vissza az előző oldalra</a>';
             }
         ?>
